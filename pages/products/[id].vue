b7e74026da20d6f8e235fff126949a89ba6b831f
Darina <dajtbaeva@gmail.com>
2023-06-14 00:51:20 +0600

    upd

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -12,6 +12,14 @@ definePageMeta({
 const { id } = useRoute().params;
 const url = "https://fakestoreapi.com/products/" + id;
 const { data: product } = await useFetch(url);
+
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Product not found",
+    fatal: true,
+  });
+}
 </script>
 
 <style scoped></style>
